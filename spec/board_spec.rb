96178cb5f94ec683940d9c6689ab4d8dd49b4ff0
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-03-26 19:31:56 +0200

    Implemented location checking

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -24,7 +24,7 @@ describe Board do
     expect(subject.retrieve column: :A, row: 1).to eq '*'
   end
 
-  it 'will not accept ship outside of the board (2x2)' do
+  it 'raises error when invalid ship location given' do
     expect do
       subject.place(column: :K, row: 2, ship: :ship)
     end.to raise_error 'not on the board'
